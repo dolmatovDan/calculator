745bfc9939397652e6d77442bc631b8053b7897f
aaarseniy <abrosimov.senya@inbox.ru>
2025-09-11 00:54:28 +0300

    Added functionality for endpoint /save

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
@@ -10,6 +10,7 @@ from typing import List, Dict, Any
 
 from database.database import init_database, DatabaseInitializationError
 from endpoints.delete import delete_all_content
+from computation.parser import Parser
 
 DB_PATH = os.path.join(os.path.dirname(__file__), "storage", "calculations.db")
 os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
@@ -53,7 +54,22 @@ _NEXT_ID = 1
 
 @app.post("/save")
 def safe_eval(expr: str) -> float:
-    raise NotImplementedError("Calculation is not implemented yet")
+    """Compute endpoint that saves the input string to database and returns result of computation"""
+    parser = Parser()
+    try:
+        result = parser.parse_expression(expr)
+        # string_id = save_string(expr)
+        # Для целочисленного деления показываем целые числа без .0 где возможно
+        if result.is_integer():
+            return int(result)
+        else:
+            return result
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=e
+        )
 
 @app.get("/health")
 def health():
